0b82e7ab725d6a5a9df022075801ace69b9b2274
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-31 01:20:14 +0530

    file removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 /* eslint-disable @next/next/link-passhref */
 import WebApi from '../components/WebApi';
-import WithPackage from '../components/WithPackage';
+
 
 export default function Home() {
   return (
     <div>
 <h1 className='text-center text-4xl font-semibold pt-10'>Next Js Text to Speech </h1>
-      
-      <WithPackage />
       <WebApi />
     </div>
   );
