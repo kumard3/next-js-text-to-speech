d1eceb573d31bf0d5f3acc2b84d0fb6c4436f1a1
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-31 01:45:04 +0530

    useSpeechSynthesisApi hook made

--- components/WebApi.tsx
+++ components/WebApi.tsx
@@ -1,62 +1,18 @@
-import { useState, useCallback } from "react";
+import { useSpeechSynthesisApi } from "../hooks/useSpeechSynthesisApi";
 
 export default function WebApi() {
-
-  const [value, setValue] = useState(
-    "Hey there! Cyphen, a community  with great passion and zest in building a healthy environment To learn •Open Source•Web Development•Data structures and Algorithms•Projects•Competitive Coding•ProgrammingMotivational talks, Opportunities, and much much more!Suggestions are always welcomed."
-  );
-
-  const [isSpeaking, setIsSpeaking] = useState(false);
-  const [isPaused, setIsPaused] = useState(false);
-  const [isResumed, setIsResumed] = useState(false);
-  const [isEnded, setIsEnded] = useState(false);
-
-
-  const createGraphics = useCallback(() => {
-    var msg = new SpeechSynthesisUtterance();
-
-    msg.text = value;
-    function speak() {
-      window.speechSynthesis.speak(msg);
-    }
-    speak();
-    setIsSpeaking(true);
-    setIsEnded(false);
-  }, [value]);
-
-  const Pause = useCallback(() => {
-    function pause() {
-      window.speechSynthesis.pause();
-    }
-    pause();
-    setIsPaused(true);
-    setIsSpeaking(false);
-    setIsEnded(false);
-    setIsResumed(false);
-  }, []);
-
-  const Resume = useCallback(() => {
-    function resume() {
-      window.speechSynthesis.resume();
-    }
-    resume();
-    setIsPaused(false);
-    setIsSpeaking(false);
-    setIsEnded(false);
-    setIsResumed(true);
-  }, []);
-
-  const Cancel = useCallback(() => {
-    function cancel() {
-      window.speechSynthesis.cancel();
-    }
-    cancel();
-    setIsPaused(false);
-    setIsResumed(false);
-
-    setIsSpeaking(false);
-    setIsEnded(true);
-  }, []);
+  const {
+    text,
+    setText,
+    isSpeaking,
+    isPaused,
+    isResumed,
+    isEnded,
+    speak,
+    pause,
+    resume,
+    cancel,
+  } = useSpeechSynthesisApi();
 
   const textarea = `bg-gray-500 text-white rounded-3xl p-[14px] text-xl w-[360px] sm:w-[30rem] min-h-[20rem]`;
 
@@ -67,26 +23,24 @@ export default function WebApi() {
       <div className="max-w-[36rem]">
         <textarea
           className={`${textarea} `}
-          value={value}
-          onChange={(event) => setValue(event.target.value)}
+          value={text}
+          onChange={(event) => setText(event.target.value)}
         />
       </div>
       <div className="w-full flex justify-center items-center">
-   
-
-        <button onClick={createGraphics} className={`${button} mx-3 `}>
+        <button onClick={speak} className={`${button} mx-3 `}>
           Speak
         </button>
         {isSpeaking ? "Speaking" : ""}
-        <button onClick={Pause} className={`${button} mx-3 `}>
+        <button onClick={pause} className={`${button} mx-3 `}>
           pause
         </button>
         {isPaused ? "Paused" : ""}
-        <button onClick={Resume} className={`${button} mx-3 `}>
+        <button onClick={resume} className={`${button} mx-3 `}>
           resume
         </button>
         {isResumed ? "Resumed" : ""}
-        <button onClick={Cancel} className={`${button} mx-3 active:  `}>
+        <button onClick={cancel} className={`${button} mx-3 active:  `}>
           cancel
         </button>
         {isEnded ? "Canceled" : ""}
--- hooks/useSpeechSynthesisApi.ts
+++ hooks/useSpeechSynthesisApi.ts
@@ -1,55 +1,9 @@
-import { useCallback,useState,useEffect } from "react";
+import { useCallback,useState } from "react";
 
 
 export const useSpeechSynthesisApi = () => {
-//     const [voices, setVoices] = useState<SpeechSynthesisVoice[]>([]);
-//     const [isSpeaking, setIsSpeaking] = useState(false);
-//     const [isPaused, setIsPaused] = useState(false);
-//     const [isStopped, setIsStopped] = useState(false);
-//     const [isEnded, setIsEnded] = useState(false);
-
-//     const getVoices = useCallback(() => {
-//         window.speechSynthesis.getVoices()
-        
-        
-//     }
-//     ,[]);
-
-//     const speak = useCallback((utterance: SpeechSynthesisUtterance) => {
-//         window.speechSynthesis.speak(utterance);
-//     }
-//     ,[]);
-
-//     const pause = useCallback(() => {
-//         window.speechSynthesis.pause();
-//     }
-//     ,[]);
-
-//     const resume = useCallback(() => {
-//         window.speechSynthesis.resume();
-//     }
-//     ,[]);
-
-//     const stop = useCallback(() => {
-//         window.speechSynthesis.cancel();
-//     }   
-//     ,[]);
-// return{
-//     voices,
-//     isSpeaking,
-//     isPaused,
-//     isStopped,
-//     isEnded,
-//     getVoices,
-//     speak,
-//     pause,
-//     resume,
-//     stop
-
-// }
-
-const [text, setText] = useState("");
-
+    
+  const [text, setText] = useState("");
   const [isSpeaking, setIsSpeaking] = useState(false);
   const [isPaused, setIsPaused] = useState(false);
   const [isResumed, setIsResumed] = useState(false);
